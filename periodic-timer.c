32625baddab98c8da8817f8481f24ce5e53becfb
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-03 20:41:37 -0300

    feat: create the timer callback function

--- periodic-timer.c
+++ periodic-timer.c
@@ -7,6 +7,30 @@
 
 int state = 0;  // Controlls the state of the traffic light
 
+//  Timer Callback
+bool repeating_timer_callback(struct repeating_timer *t) {
+    printf("3 segundos passaram\n");
+
+    // Turn off all LEDs before turning on the next one
+    gpio_put(LED_PIN_RED, 0);
+    gpio_put(LED_PIN_YELLOW, 0);
+    gpio_put(LED_PIN_GREEN, 0);
+
+    // Alternate between the LEDs based on the state
+    if (state == 0) {
+        gpio_put(LED_PIN_RED, 1);   // Vermelho
+    } else if (state == 1) {
+        gpio_put(LED_PIN_YELLOW, 1); // Amarelo
+    } else if (state == 2) {
+        gpio_put(LED_PIN_GREEN, 1); // Verde
+    }
+
+    // Advance the state
+    state = (state + 1) % 3;
+
+    return true;
+}
+
 
 int main()
 {
