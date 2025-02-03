e02aaa15a7dec87b465a57812fd2201c36bd73b9
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-03 20:37:05 -0300

    feat: initialize the GPIO pins for the LEDs

--- periodic-timer.c
+++ periodic-timer.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_PIN_RED 11
+#define LED_PIN_YELLOW 12
+#define LED_PIN_GREEN 13
 
 
 int main()
 {
     stdio_init_all();
+
+    // Initialize the GPIO pins for the LEDs
+    gpio_init(LED_PIN_RED);
+    gpio_set_dir(LED_PIN_RED, true);
+    gpio_init(LED_PIN_YELLOW);
+    gpio_set_dir(LED_PIN_YELLOW, true);
+    gpio_init(LED_PIN_GREEN);
+    gpio_set_dir(LED_PIN_GREEN, true);
 
     while (true) {
         printf("Hello, world!\n");
